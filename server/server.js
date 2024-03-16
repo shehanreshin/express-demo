168cfe006a6b088459b8e65bac1b5e050a5f7af0
shehanreshin <asp.shehanreshin@gmail.com>
2024-03-16 18:31:16 +0530

    Create skeleton endpoints for basic CRUD operations

--- server/server.js
+++ server/server.js
@@ -5,3 +5,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('/api/v1/contacts', (req, res) => {
+    res.status(200).json({ message: 'Get all contacts' });
+});
+
+app.post('/api/v1/contacts', (req, res) => {
+    res.status(200).json({ message: 'Contact saved' })
+});
+
+app.delete('/api/v1/contacts/:id', (req, res) => {
+    res.status(200).json({ message: `Contact id: ${req.params.id} deleted` })
+});
+
+app.put('/api/v1/contacts/:id', (req, res) => {
+    res.status(200).json({ message: `Contact id: ${req.params.id} updated` })
+});
