aafd520beefb52d21b7e5277111d72cc19b7a6da
shehanreshin <asp.shehanreshin@gmail.com>
2024-03-23 21:33:31 +0530

    Complete exception handler and separate constants into a single file for maintainability

--- server/controller/book-controller.js
+++ server/controller/book-controller.js
@@ -1,26 +1,28 @@
+const { constants } = require("../util/constant");
+
 const createBook = (req, res) => {
     const { title, author, publisher } = req.body;
     if (!title || !author || !publisher) {
-        res.status(400);
+        res.status(constants.VALIDATION_ERROR);
         throw new Error("Ensure that all the fields are filled");
     }
-    res.status(201).json({ message: 'Create book' });
+    res.status(constants.CREATED).json({ message: 'Create book' });
 };
 
 const getBooks = (req, res) => {
-    res.status(200).json({ message: 'Get all books' });
+    res.status(constants.OK).json({ message: 'Get all books' });
 };
 
 const getBook = (req, res) => {
-    res.status(200).json({ message: `Get book id: ${req.params.id}` });
+    res.status(constants.OK).json({ message: `Get book id: ${req.params.id}` });
 };
 
 const updateBook = (req, res) => {
-    res.status(200).json({ message: `Book id: ${req.params.id} updated` });
+    res.status(constants.OK).json({ message: `Book id: ${req.params.id} updated` });
 };
 
 const deleteBook = (req, res) => {
-    res.status(200).json({ message: `Book id: ${req.params.id} deleted` });
+    res.status(constants.OK).json({ message: `Book id: ${req.params.id} deleted` });
 };
 
 module.exports = {
---
+++ server/util/constant.js
@@ -0,0 +1,10 @@
+module.exports.constants = {
+    OK: 200,
+    CREATED: 201,
+    ACCEPTED: 202,
+    VALIDATION_ERROR: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_ERROR: 500,
+};
