5d187a676e014b36ddebb1b9763b58587bd8c683
shehanreshin <asp.shehanreshin@gmail.com>
2024-03-24 19:04:45 +0530

    Add functionality to create and get all the books by interacting with the database

--- server/controller/book-controller.js
+++ server/controller/book-controller.js
@@ -1,5 +1,6 @@
 const { constants } = require("../util/constant");
 const asyncHandler = require('express-async-handler');
+const Book = require('../model/book-model');
 
 const createBook = asyncHandler(async (req, res) => {
     const { title, author, publisher } = req.body;
@@ -7,11 +8,15 @@ const createBook = asyncHandler(async (req, res) => {
         res.status(constants.VALIDATION_ERROR);
         throw new Error("Ensure that all the fields are filled");
     }
-    res.status(constants.CREATED).json({ message: 'Create book' });
+
+    res.status(constants.CREATED).json(await Book.create({
+        title, author, publisher,
+    }));
 });
 
 const getBooks = asyncHandler(async (req, res) => {
-    res.status(constants.OK).json({ message: 'Get all books' });
+    const books = await Book.find();
+    res.status(constants.OK).json(books);
 });
 
 const getBook = asyncHandler(async (req, res) => {
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(errorHandler);
 app.use('/api/v1/books', require('./route/book-routes'));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
