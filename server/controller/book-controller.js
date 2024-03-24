bc168d400a3726228163debd041782c3f130b000
shehanreshin <asp.shehanreshin@gmail.com>
2024-03-24 16:46:28 +0530

    Add async handler to handle exceptions inside the async express routes by passing them to the global error handler

--- server/controller/book-controller.js
+++ server/controller/book-controller.js
@@ -1,29 +1,30 @@
 const { constants } = require("../util/constant");
+const asyncHandler = require('express-async-handler');
 
-const createBook = (req, res) => {
+const createBook = asyncHandler(async (req, res) => {
     const { title, author, publisher } = req.body;
     if (!title || !author || !publisher) {
         res.status(constants.VALIDATION_ERROR);
         throw new Error("Ensure that all the fields are filled");
     }
     res.status(constants.CREATED).json({ message: 'Create book' });
-};
+});
 
-const getBooks = (req, res) => {
+const getBooks = asyncHandler(async (req, res) => {
     res.status(constants.OK).json({ message: 'Get all books' });
-};
+});
 
-const getBook = (req, res) => {
+const getBook = asyncHandler(async (req, res) => {
     res.status(constants.OK).json({ message: `Get book id: ${req.params.id}` });
-};
+});
 
-const updateBook = (req, res) => {
+const updateBook = asyncHandler(async (req, res) => {
     res.status(constants.OK).json({ message: `Book id: ${req.params.id} updated` });
-};
+});
 
-const deleteBook = (req, res) => {
+const deleteBook = asyncHandler(async (req, res) => {
     res.status(constants.OK).json({ message: `Book id: ${req.params.id} deleted` });
-};
+});
 
 module.exports = {
     createBook, getBooks, getBook, updateBook, deleteBook
