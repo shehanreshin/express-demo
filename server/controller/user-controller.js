b09daa1c34729ed563dcd54d5b09b99401abe7d0
shehanreshin <asp.shehanreshin@gmail.com>
2024-03-25 21:36:31 +0530

    Add functionality to endpoints in user controller to register a user and get all the users by interacting with the db

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,12 +1,21 @@
 const asyncHandler = require('express-async-handler');
 const { constants } = require('../util/constant');
+const User = require('../model/user-model');
 
 const getUsers = asyncHandler(async (req, res) => {
-    res.status(constants.OK).json({ message: "all users" });
+    res.status(constants.OK).json(await User.find());
 });
 
 const register = asyncHandler(async (req, res) => {
-    res.status(constants.OK).json({ message: "registered user" });
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(constants.VALIDATION_ERROR);
+        throw new Error("Ensure that all the fields are filled");
+    }
+
+    res.status(constants.CREATED).json(await User.create({
+        username, email, password
+    }));
 });
 
 const login = asyncHandler(async (req, res) => {
